3fe1ad42369f93b19f1e38d9285ae4b9161eed00
Michael-Lipov <darii.lipov/michael.lipov@gmail.com>
2020-04-18 14:45:21 +0300

    First commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
-        int price = 10000;
-        int miles = service.calculate(20);
+        int miles = service.calculate(10000);
         System.out.println(miles);
     }
 }
